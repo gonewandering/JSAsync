e626bc26c93ad202685127b1bc9fedbbe46d0808
Lindsay Silver <lgsilver@gmail.com>
2013-03-05 12:20:08 -0800

    Initial Commit for JSAsync

---
+++ config.js
@@ -0,0 +1,13 @@
+$loader.load([
+	{
+		"key": "jquery",
+		"includes": [
+			"http://code.jquery.com/jquery-1.9.1.min.js"
+		],
+		"complete": "$('document').ready(function () { $('body').css('background', 'black'); });"
+	},
+	{
+		"key": "google-analytics",
+		"complete": "var _gaq = _gaq || []; _gaq.push(['_setAccount', 'UA-XXXXX-X']); _gaq.push(['_trackPageview']); (function() { var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true; ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js'; var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s); })();"
+	}
+]);
---
+++ load.js
@@ -0,0 +1,88 @@
+var $loader = {
+	modules:{},
+	module: function (attr) { 
+	
+		var self = this;
+		
+		// Set Module Defaults
+		self.data = { 	
+			key: null,					// Default Attributes for module
+			active: 1,					// Checks whether the item is active
+			ready: 0,
+			sample: .8,					// Percent of the time that it gets run
+			includes: [],				// External script includes
+			scripts: 0, 				// Returns the number of loaded scripts
+			complete: null				// Callback actions after scripts have been loaded
+		}
+		
+		// Add attributes to module
+		for (var at in self.data) { self.data[at] = attr[at] ? attr[at] : self.data[at]; }
+		
+		// Load Module based on attributes
+		self.load = function ()  {
+			if (self.data.includes.length) {
+				var i = 0; while(i < self.data.includes.length) {
+					var src = self.data.includes[i]; 
+					
+					self.data.includes[i] = document.createElement('script');
+					self.data.includes[i].async = true;
+					self.data.includes[i].type = 'text/javascript';
+					self.data.includes[i].src = src;
+					
+					self.data.includes[i].onload = self.data.includes[i].onreadystatechange = function () { 
+						if (!self.ready && (!self.data.readyState || self.data.readyState == 'complete')) {
+							self.data.scripts++;
+							if (self.data.scripts == self.data.includes.length) {
+								if (typeof(self.data.complete) == "function"){
+									self.data.complete();
+								} else {
+									var complete = new Function(self.data.complete);
+									complete();
+								}
+								self.ready = true;
+							} 
+						}				
+					}
+					
+					document.body.appendChild(self.data.includes[i]);
+					i++;
+				}
+			} else { 
+				if (typeof(self.data.complete) == "function"){
+					self.data.complete();
+				} else {
+					var complete = new Function(self.data.complete);
+					complete();
+				}
+				self.ready = true;			
+			}
+		}
+		
+		// Log the module for debuggin
+		this.log = function () { window.console.log(this); };
+		
+		// Initialize Module
+		self.load();
+	},
+	
+	loaded: function () {
+		var modules = [];
+		var i = 0;
+		while (i < this.modules.length) { modules[i] = this.modules[i].data.key; i++; }
+		return modules;
+	},
+	
+	load: function (module) { 
+		var self = this;
+		if (module instanceof Array) {
+			var i = 0; while (i < module.length) { 
+				self.modules[module[i].key] = new this.module(module[i]);
+				i++;
+			}
+		} else {
+			self.modules[module.key] = new this.module(module);
+		}
+	},
+	
+	log: function () { window.console.log(this); }
+};
